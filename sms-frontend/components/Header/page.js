dbaf8c2c68a0617a725b1f9cdbbf70a80eb058cd
Abhinav Dubey <dubeyabhinav179@gmail.com>
2024-02-02 21:16:22 +0530

    Mobile View Links

--- sms-frontend/components/Header/page.js
+++ sms-frontend/components/Header/page.js
@@ -204,13 +204,13 @@ export default function page() {
               </div>
               <div className="py-6">
               <Link
-                  href="#"
+                  href="/sign-in"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 hover:bg-[#8f3ce2] text-white bg-[#841FEB]"
                 >
                   Sign Up
                 </Link>
                 <Link
-                  href="#"
+                  href="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
