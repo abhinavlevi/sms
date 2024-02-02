5be9bb8d4f6e1c4a503bf154ea0bb61e5bb1eacb
Abhinav Dubey <dubeyabhinav179@gmail.com>
2024-02-02 21:11:16 +0530

    Social Share Image and css issues

--- sms-frontend/app/layout.js
+++ sms-frontend/app/layout.js
@@ -6,6 +6,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "XanForm - ERP Solution",
   description: "XanForm software is a complete ERP solution specially developed for techno-savvy Schools, colleges and universities. This advanced software has all the features incorporated which are needed to handle all the required activities. School & College can manage activities like student attendance, fee, exam, library records and much more.",
+  openGraph: {
+    images: [
+      {
+        url: 'https://opengraph.b-cdn.net/production/documents/a83f6ebe-4aba-433b-9f9c-96f90f8b1d06.png?token=yyC-wXk6kcKGqPbDeVxX2is-jVP9eOnu9cBwsqZ0JFY&height=1080&width=1080&expires=33242888420',
+        alt: 'XanForm opengraph Image',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
